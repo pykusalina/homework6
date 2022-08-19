3c0c3de41cbed613795524d70c557f1f69f2d797
Alina Pykus <alinapykus17@email.com>
2022-08-19 04:27:14 +0300

    add some changes

---
+++ main.js
@@ -0,0 +1,51 @@
+// Task 1
+// let element = document.getElementById("test");
+// element.innerHTML = "last";
+
+// document.getElementById("text").innerHTML = "LAst";
+
+// task 2
+// document.getElementById("image").src="cat.jpg";
+
+// Task 3
+// let text = document.querySelectorAll("p");
+// console.log(text);
+
+// Task 4
+// let list = document.getElementById('list');
+// alert(list.firstChild);
+// alert(list.lastChild);
+// alert(list.childNodes[1]);
+// alert(list.childNodes[4]);
+// alert(list.childNodes[6]);
+
+// Task5
+// let header = document.querySelector("h1");
+// header.style.background = "green";
+// let p1 = document.querySelector("myDiv").firstChild;
+// p1.style.fontSize = "24px";
+// let p2 = document.querySelector("muDiv").childNodes[1];
+// p2.style.textDecoration = "underline";
+// let p3 = document.querySelector("myDiv").childNodes[2];
+// p3.style.color = "red";
+// let p4 = document.querySelector("myDiv").childNodes[3];
+// let el = document.getElementsByTagName("span");
+// el.style.visibility = "visible";
+
+// TAsk 6
+// let question1 = prompt('text something');
+// let question2 = prompt('text something');
+
+// let value1 = document.forms[0] = question1;
+// let value2 = document.forms[1] = question2;
+
+// value1 = question2;
+// value2 = question1;
+
+// TAsk 7
+// let newTag = document.createElement("main");
+// newTag.className = "mainClass check item";
+// let div = document.createElement("div");
+// div.id = "myDiv";
+// let p = document.createElement("p");
+// p.innerHTML = "First paragraph"
